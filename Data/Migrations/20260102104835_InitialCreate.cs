9660299d354efac4669714ef128e5e0e382c494d
Umutcrn <umut.ceren@ef.karaelmas.edu.tr>
2026-01-02 13:52:28 +0300

    Initial migration added

---
+++ Data/Migrations/20260102104835_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BirikimPro.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AylikBirikim",
+                table: "Hedefler");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileImage",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "AylikBirikim",
+                table: "Hedefler",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileImage",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
